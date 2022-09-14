4f830085c9651fec52127f70d27c664c7f3687ed
Mantas <mantaspauza@gmail.com>
2022-09-14 18:31:53 +0300

    feat: :sparkles: add display of choices
    added a div that shows what you and computer chose

--- script.js
+++ script.js
@@ -9,13 +9,10 @@ const buttons = document.querySelectorAll(".selection");
 const playerScore = document.querySelector(".score_text_player");
 const computerScore = document.querySelector(".score_text_computer");
 const output = document.querySelector(".output");
+const subOutput = document.querySelector(".sub_output");
 const playButton = document.querySelector("#play");
 
 computerSelection = getComputerChoise();
-
-function changeContainerStyle() {
-  pyro.classList.toggle("pyro");
-}
 
 let playerResult = 0;
 let computerResult = 0;
@@ -23,13 +20,17 @@ let computerResult = 0;
 playButton.addEventListener("click", function () {
   scoreContainer.style.display = "flex";
   outputContainer.style.display = "flex";
+  outputContainer.style.flexDirection = "column";
   selectionContainer.style.display = "flex";
+  outputContainer.style.textAlign = "center";
+  subOutput.style.textAlign = "center";
   titleContainer.style.display = "none";
   output.textContent = ``;
+  subOutput.textContent = ``;
   pyro.classList.remove("pyro");
   playButton.style.display = "none";
-  title.classList.add('animate__fadeInDown');
-  title.classList.remove('animate__hinge');
+  title.classList.add("animate__fadeInDown");
+  title.classList.remove("animate__hinge");
 });
 
 buttons.forEach((button) => {
@@ -40,12 +41,12 @@ buttons.forEach((button) => {
 
     if (playerResult === 5) {
       changeContainerStyle();
-      title.classList.add('animate__fadeInDown');
-        title.classList.remove('animate__hinge');
+      title.classList.add("animate__fadeInDown");
+      title.classList.remove("animate__hinge");
     }
     if (computerResult === 5) {
-    title.classList.remove('animate__fadeInDown');
-    title.classList.add('animate__hinge');
+      title.classList.remove("animate__fadeInDown");
+      title.classList.add("animate__hinge");
     }
     if (playerResult === 5 || computerResult === 5) {
       returnWinner();
@@ -89,6 +90,8 @@ function playRound(playerSelection, computerSelection) {
     result = "Tie";
     output.style.color = "black";
     output.textContent = `It's a tie!`;
+    subOutput.textContent = `You both chose ${playerSelection}!`;
+    subOutput.style.fontSize = "12px";
   } else if (
     (playerSelection === "Rock" && computerSelection === "Paper") ||
     (playerSelection === "Scissors" && computerSelection === "Rock") ||
@@ -97,6 +100,8 @@ function playRound(playerSelection, computerSelection) {
     result = "You lose!";
     output.style.color = "red";
     output.textContent = `Aww, ${computerSelection} wins over ${playerSelection}! :(`;
+    subOutput.textContent = `Computer chose ${computerSelection} and you chose ${playerSelection}.`;
+    subOutput.style.fontSize = "12px";
     computerResult++;
     computerScore.textContent = computerResult;
   } else {
@@ -105,6 +110,12 @@ function playRound(playerSelection, computerSelection) {
     playerScore.textContent = playerResult;
     output.style.color = "green";
     output.textContent = `Congratulations! ${playerSelection} wins over ${computerSelection}! :)`;
+    subOutput.textContent = `Computer chose ${computerSelection} and you chose ${playerSelection}.`;
+    subOutput.style.fontSize = "12px";
   }
   return result;
+}
+
+function changeContainerStyle() {
+  pyro.classList.toggle("pyro");
 }
